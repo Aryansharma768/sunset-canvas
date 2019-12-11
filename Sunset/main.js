39095d5f172d0a18bd7591ae9c2efc78614e03a2
Aryansharma768 <55804095+Aryansharma768@users.noreply.github.com>
2019-12-11 10:22:15 -0700

    Add files via upload

---
+++ Sunset/main.js
@@ -0,0 +1,30 @@
+let cnv = document.getElementById("myCanvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 400;
+cnv.height = 400;
+
+let cloudLogoImg = document.getElementById("cloud-logo-img");
+
+//Background
+ctx.fillStyle = "blue";
+ctx.fillRect(0, 0, 400, 300);
+
+
+
+
+//circles
+ctx.linewidth = 5;
+ctx.fillStyle = "red";
+ctx.beginPath();
+ctx.arc(200, 300, 20, 0, 2 * Math.PI);
+ctx.fill();
+
+
+
+//Draw images
+ctx.drawImage(cloudLogoImg,150,100);
+ctx.drawImage(cloudLogoImg,175,90);
+
+
+ctx.fillStyle ="green"
+ctx.fillRect(0,300,400,300);
